802d751880e267aee5168c74cbd2fd17d21ae499
JOAO CARLOS <jc8055839@gmail.com>
2026-08-17 14:30:40 -0300

    atvdade01.c

--- AED1/atvdade01.c
+++ AED1/atvdade01.c
@@ -2,21 +2,21 @@
 # include <stdio.h>
 int main(){
     int vida = 100;
-    int tesouros = 0; // 0=inativo 1=ativo;
+    int tesouros = 0;
     int *pvida , *ptesouros;
 
     pvida = &vida;
     ptesouros = &tesouros;
 
     printf("VIDA ATUAL: %d\n", *pvida);
-    printf("o personagem sofreu 30 de dano.\n");
+    printf("O personagem sofreu 30 de dano.\n");
     printf("O ENDEREÇO DA VIDA É %p\n: ", pvida);
     *pvida= *pvida - 30; 
     printf("VIDA APOS O DANO: %d\n", *pvida);
 
     printf("\n\n");
 
-    printf("apos o dano o personagem achou um alimento, que tem poder de cura, o personagem come e recupera 1/2 do dano sofrido.\n");
+    printf("Apos o dano o personagem achou um alimento, que tem poder de cura, o personagem come e recupera 1/2 do dano sofrido.\n");
     printf("O ENDEREÇO DA VIDA É %p\n: ", pvida);
     *pvida = *pvida + 15;
     printf("VIDA APOS O ATRIBUTO DO ALIMENTO: %d\n", *pvida);
