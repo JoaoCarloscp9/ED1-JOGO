eee4610a07602bf155428811ac5c67ca10d738c9
JOAO CARLOS <jc8055839@gmail.com>
2026-08-17 14:57:59 -0300

    atvdade01.c

--- AED1/atvdade01.c
+++ AED1/atvdade01.c
@@ -18,6 +18,7 @@ int main(){
 
     printf("Apos o dano o personagem achou um alimento, que tem poder de cura, o personagem come e recupera 1/2 do dano sofrido.\n");
     printf("O ENDEREÇO DA VIDA É %p\n: ", pvida);
+    printf("VIDA ATUAL: %d\n", *pvida);
     *pvida = *pvida + 15;
     printf("VIDA APOS O ATRIBUTO DO ALIMENTO: %d\n", *pvida);
     printf("APOS SUA CAMINHADA ELE ACHOU O TESOURO.\n");
